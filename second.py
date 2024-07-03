4b25d2ed23f364f19047e1d13ed185306c5c48a4
jayasri7d <dakarapujayasri05@gmail.com>
2024-07-03 22:36:09 +0530

    fresher

--- second.py
+++ second.py
@@ -1,3 +1,9 @@
-print('Hello')
-print('Hello World')
-print('This is python ')
+meet=input("are you fresher?")
+if meet=="yes":
+   print('Hello')
+   print('Hello World')
+   print('This is python')
+else:
+    change=input("are you not a fresher?")
+    if change=="yes":
+        print("I am not a fresher")
