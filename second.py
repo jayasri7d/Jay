ce219dcf8c63de0a4a30b66b042df5081832e841
jayasri7d <dakarapujayasri05@gmail.com>
2024-07-04 00:16:10 +0530

    Revert "fresher"
    This reverts commit 4b25d2ed23f364f19047e1d13ed185306c5c48a4.

--- second.py
+++ second.py
@@ -1,9 +1,3 @@
-meet=input("are you fresher?")
-if meet=="yes":
-   print('Hello')
-   print('Hello World')
-   print('This is python')
-else:
-    change=input("are you not a fresher?")
-    if change=="yes":
-        print("I am not a fresher")
+print('Hello')
+print('Hello World')
+print('This is python ')
